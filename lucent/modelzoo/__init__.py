a3cc24317ff6da12b2b0ab50b880afcadc60de5c
atagade <atagade19@gmail.com>
2023-02-06 13:03:40 +1100

    Experimenting with __init__.py in lucent.modelzoo

--- lucent/modelzoo/__init__.py
+++ lucent/modelzoo/__init__.py
@@ -1,3 +1,3 @@
 from lucent.modelzoo.inceptionv1 import InceptionV1 as inceptionv1
-from lucent.modelzoo.custom_resnet import resnet as custom_resnet
+from lucent.modelzoo.custom_resnet import custom_resnet as custom_resnet
 from torchvision.models import *
